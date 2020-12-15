85caa886948313f9b04da77f69dc94d695e18dd3
Oleksandr <sanyazov@ukr.net>
2020-12-15 15:32:24 +0200

    123

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <Header />
       <p>Voshel v Lehu</p>
-<<<<<<< HEAD
             <p>Nu sps paluchaetsa</p>
-=======
->>>>>>> Voshel_v_Lehu
     </div>
   );
 }
