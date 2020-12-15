de6dc0940b59d57f32b755a2d26bda5a593bb29b
Spike <ololmkbc@gmail.com>
2020-12-15 14:56:31 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-ClasstimeKiller
+# ClassimeKiller
+ time to kill classtime
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
