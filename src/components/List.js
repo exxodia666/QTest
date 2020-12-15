90857fc82d2fafb53a547b80317cc1403e963818
Spike <ololmkbc@gmail.com>
2020-12-15 21:18:50 +0200

    Redux connected to UI

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,22 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { loadQuizzes } from "../redux/actions/show_quizzes";
 
 function List() {
-  const [todos, setTodos] = useState(null);
+  //const [todos, setTodos] = useState(null);
 
+  //ТУТ подключен редакс стор
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.quizzes);
+  //ТУТ ГРУЗЯТСЯ ТЕСТЫ
+  useEffect(() => {
+    dispatch(loadQuizzes("todos"));
+  }, [dispatch]);
+
+  console.log(state);
+  /*
   const fetchData = async () => {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/todos"
@@ -13,24 +25,29 @@ function List() {
     setTodos(response.data);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  }, [])
-
-  return (
-    <div>
-      <ListGroup >
-        {todos &&
-          todos.map((todo, index) => {
-            return (
-              <ListGroup.Item action variant="light">
-                {index + 1} - {todo.title}
-              </ListGroup.Item>
-            );
-          })}
-      </ListGroup>
-    </div>
-  );
+  }, []);
+  */
+  //НУЖНО ОТРЕНДЕРИТЬ state.questions
+  if (state.questions.length) {
+    return (
+      <div>
+        <ListGroup>
+          {todos &&
+            todos.map((todo, index) => {
+              return (
+                <ListGroup.Item action variant="light">
+                  {index + 1} - {todo.title}
+                </ListGroup.Item>
+              );
+            })}
+        </ListGroup>
+      </div>
+    );
+  } else {
+    return <p>Loading!!!</p>;
+  }
 }
 
 export default List;
