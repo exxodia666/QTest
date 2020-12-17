35ac59e5e718b25833fcc6ad6bd181d87888ee8f
Spike <ololmkbc@gmail.com>
2020-12-17 11:32:46 +0200

    Auto stash before merge of "Merge" and "Redux"

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import quizzes from "./quizzes/quizzes";
+import results from "./results/results";
+import answers from "./answers/answer";
 
 export default combineReducers({
   quizzes,
+  results,
+  answers,
 });
--- src/redux/reducers/quizzes/quizzes.js
+++ src/redux/reducers/quizzes/quizzes.js
@@ -1,11 +1,27 @@
-import { SHOW_QUIZZES } from "../../actions/show_quizzes";
+import { SHOW_QUIZZES, SET_DONE } from "../../actions/show_quizzes";
 
-const initialState = { questions: [] };
+const initialState = { quiz: {}, questions: [] };
 // eslint-disable-next-line
 export default (state = initialState, action) => {
   switch (action.type) {
     case SHOW_QUIZZES:
-      return { ...action.payload };
+      console.log(action.payload);
+      const newArray = action.payload.questions.map((item) => {
+        const temp = { ...item, isDone: false };
+        return temp;
+      });
+      console.log(newArray);
+      return { ...action.payload, questions: newArray };
+    case SET_DONE:
+      ///TODO REFUCKTOR
+      const found = state.questions.find(
+        (element) => element.question.id === action.payload
+      );
+      const index = state.questions.indexOf(found);
+      const newState = { ...state };
+      newState.questions[index].isDone = true;
+      console.log(newState);
+      return { ...newState };
     default:
       return state;
   }
