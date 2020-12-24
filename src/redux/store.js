9faf9e54433f1ad9d677a0362b911b461d37e091
Oleksandr <sanyazov@ukr.net>
2020-12-24 23:52:44 +0200

    add new quiz: form

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ const store = createStore(
   persistedState,
   compose(
     applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
