38c0c4ffaeb3518f09f37e3c69173fca91b8c9a3
Exxodia666 <Exxodia666@gmail.com>
2021-01-15 19:52:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { Provider } from "react-redux";
-import { Profiler } from "react";
-import store from "./redux/store";
-import MainNavigator from "./navigation/MainNavigator";
-//import ImageComponent from "./navigation/Image";
+// import { Provider } from "react-redux";
+// import { Profiler } from "react";
+// import store from "./redux/store";
+// import MainNavigator from "./navigation/MainNavigator";
+// //import ImageComponent from "./navigation/Image";
 
 function App() {
-  const callback = (id, phase, actualTime, baseTime, startTime, commitTime) => {
-    // console.log(`${id}'s ${phase} phase:`);
-    // console.log(`Actual time: ${actualTime}`);
-    // console.log(`Base time: ${baseTime}`);
-    // console.log(`Start time: ${startTime}`);
-    // console.log(`Commit time: ${commitTime}`);
-  };
-  console.log("RENDER APP");
   return (
-    <Provider store={store}>
-      <Profiler id="Quizzes" onRender={callback}>
-        <MainNavigator />
-      </Profiler>
-    </Provider>
+    <p>TEST</p>
+    // <Provider store={store}>
+    //   <Profiler id="Quizzes" onRender={callback}>
+    //     <MainNavigator />
+    //   </Profiler>
+    // </Provider>
   );
 }
 
