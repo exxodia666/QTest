f10cca87db66ac1d37211c4aa32e7eee31c59658
Spike <ololmkbc@gmail.com>
2021-01-15 19:25:57 +0200

    + hashrouter

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "../components/Header/Header";
 import HomeScreen from "./Screens.js/HomeScreen";
 import Quiz from "./Screens.js/Quiz";
