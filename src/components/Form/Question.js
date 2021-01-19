70a3df83648f4980a0735e0a6a12019639fbc7ea
Spike <ololmkbc@gmail.com>
2021-01-19 17:40:02 +0200

    image bug fix

--- src/components/Form/Question.js
+++ src/components/Form/Question.js
@@ -1,128 +1,123 @@
-import Axios from "axios";
-import React, { useState } from "react";
-import Choice from "./Choice";
-import "./global-slave.css";
+import Axios from 'axios';
+import React, { useState } from 'react';
+import Choice from './Choice';
+import './global-slave.css';
 
-export default function QuestionComponent({
-  addAnswers,
-  question_id,
-  choices,
-}) {
-  function deleteImage() {
-    return Axios({
-      method: "delete",
-      url: `https://questimie.herokuapp.com/api/image/${id}`,
-      data: {
-        editing_key: key,
-      },
-    }).then((e) => {
-      setImage(null);
-      setId(null);
-      setKey(null);
-      console.log(e);
-    });
-  }
+export default function QuestionComponent({ addAnswers, question_id, choices, setArray }) {
+	const [image, setImage] = useState(null);
+	const [id, setId] = useState(null);
+	const [key, setKey] = useState(null);
 
-  const sendImages = (selectedFile) => {
-    const formdata = new FormData();
-    formdata.append("picture", selectedFile);
-    return Axios({
-      method: "post",
-      url: "https://questimie.herokuapp.com/api/image",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      data: formdata,
-    })
-      .then((e) => {
-        setKey(e.data.editing_key);
-        setImage(e.data.picture);
-        setId(e.data.id);
-      })
-      .catch((e) => console.log(e));
-  };
+	function deleteImage() {
+		return Axios({
+			method: 'delete',
+			url: `https://questimie.herokuapp.com/api/image/${id}`,
+			data: {
+				editing_key: key,
+			},
+		}).then((e) => {
+			setImage(null);
+			setId(null);
+			setKey(null);
+			console.log(e);
+		});
+	}
+	const sendImages = (selectedFile) => {
+		const formdata = new FormData();
+		formdata.append('picture', selectedFile);
+		return Axios({
+			method: 'post',
+			url: 'https://questimie.herokuapp.com/api/image',
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+			data: formdata,
+		})
+			.then((e) => {
+				setKey(e.data.editing_key);
+				setImage(e.data.picture);
+				setId(e.data.id);
+			})
+			.catch((e) => console.log(e));
+	};
+	return (
+		<div className="question_container">
+			<div className="question-title_container">
+				<p>Вопрос {question_id + 1}</p>
+			</div>
+			<div className="question-name_container">
+				<input
+					type="text"
+					className="text-input"
+					placeholder="Название вопроса"
+					name={`question-wording-${question_id}`}
+				/>
+			</div>
 
-  const [image, setImage] = useState(null);
-  const [id, setId] = useState(null);
-  const [key, setKey] = useState(null);
-  return (
-    <div className="question_container">
-      <div className="question-title_container">
-        <p>Вопрос {question_id + 1}</p>
-      </div>
-      <div className="question-name_container">
-        <input type="text" className="text-input" placeholder="Название вопроса" name={`question-wording-${question_id}`}/>
-      </div>
+			{!image && (
+				<div className="add-image_container">
+					<div className="form-group">
+						<input
+							onChange={(e) => {
+								sendImages(e.target.files[0]);
+							}}
+							type="file"
+							name={`question-file-${question_id}`}
+							id={`question-file-${question_id}`}
+							className="input-file"
+						/>
+						<label htmlFor={`question-file-${question_id}`} className="btn-tertiary">
+							<div></div>
+							<span className="js-fileName">Загрузить картинку</span>
+						</label>
+					</div>
+				</div>
+			)}
 
-      {!image && (
-        <div className="add-image_container">
-          <div className="form-group">
-            <input
-              onChange={(e) => {
-                sendImages(e.target.files[0]);
-              }}
-              type="file"
-              name="file"
-              id="file"
-              className="input-file"
-            />
-            <label for="file" className="btn-tertiary">
-              <div></div>
-              <span className="js-fileName">Загрузить картинку</span>
-            </label>
-          </div>
-        </div>
-      )}
+			{image && (
+				<div className="img_container">
+					<div className="img-wrap">
+						<img src={image} className="img" />
+						<div onClick={deleteImage} className="del-img-btn">
+							<div></div>
+						</div>
+					</div>
+				</div>
+			)}
 
-      {image && (
-        <div className="img_container">
-          <div className="img-wrap">
-            <img src={image} className="img" />
-            <div onClick={deleteImage} className="del-img-btn">
-              <div></div>
-            </div>
-          </div>
-        </div>
-      )}
-
-      <div className="description_container">
-        <textarea
-          placeholder="Описание"
-          rows="6"
-          className="description"
-          name={`question-text-${question_id}`}
-        ></textarea>
-      </div>
-      <input
-        disabled={true}
-        type="hidden"
-        value={id}
-        name={`question-image-${question_id}`}
-      />
-      <div className="answers_container">
-        {choices.map((el) => {
-          return <Choice question_id={question_id} choise_id={el.choise_id} />;
-        })}
-      </div>
-      <div className="add-answer-button_container">
-        <div
-          onClick={() => {
-            addAnswers(question_id);
-          }}
-          className="add-answer-button add-button button"
-        >
-          <div className="add-img">
-            <div></div>
-          </div>
-          <p>Добавить ответ</p>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="description_container">
+				<textarea
+					placeholder="Описание"
+					rows="6"
+					className="description"
+					name={`question-text-${question_id}`}
+				></textarea>
+			</div>
+			<input type="hidden" value={id} name={`question-image-${question_id}`} />
+			<div className="answers_container">
+				{choices.map((el) => {
+					return <Choice question_id={question_id} choise_id={el.choise_id} />;
+				})}
+			</div>
+			<div className="add-answer-button_container">
+				<div
+					onClick={() => {
+						addAnswers(question_id);
+					}}
+					className="add-answer-button add-button button"
+				>
+					<div className="add-img">
+						<div></div>
+					</div>
+					<p>Добавить ответ</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 {
-  /* <div id={`question${question_id}`}>
+	/* <div id={`question${question_id}`}>
       {image && <img src={"http://134.249.181.40:7777" + image} />}
       <input
         type="text"
--- src/navigation/Screens.js/AddQuizScreen.js
+++ src/navigation/Screens.js/AddQuizScreen.js
@@ -1,220 +1,198 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { add_test, reset_add_test } from "../../redux/actions/add_test";
-import QuestionComponent from "../../components/Form/Question";
-import "./styles/global-master.css";
-import { Container } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
-import "./stylesforhomescreen/autorization.css";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add_test, reset_add_test } from '../../redux/actions/add_test';
+import QuestionComponent from '../../components/Form/Question';
+import './styles/global-master.css';
+import { Container } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import './stylesforhomescreen/autorization.css';
 
 export default function AddQuizScreen() {
-  const dispatch = useDispatch();
-  const test_status = useSelector((state) => state.add_test);
-  const history = useHistory();
-  const [array, setArray] = useState([
-    {
-      question_id: 0,
-      choises: [
-        {
-          choise_id: 0,
-          isCorect: false,
-        },
-        {
-          choise_id: 1,
-          isCorect: false,
-        },
-      ],
-    },
-  ]);
+	const dispatch = useDispatch();
+	const test_status = useSelector((state) => state.add_test);
+	const history = useHistory();
+	const [array, setArray] = useState([
+		{
+			question_id: 0,
+			image: '',
+			choises: [
+				{
+					choise_id: 0,
+					isCorect: false,
+				},
+				{
+					choise_id: 1,
+					isCorect: false,
+				},
+			],
+		},
+	]);
 
-  const user = useSelector((state) => state.user.loggedIn);
+	const user = useSelector((state) => state.user.loggedIn);
 
-  if (!user) {
-    history.push("/");
-  }
+	if (!user) {
+		history.push('/');
+	}
 
-  React.useEffect(() => {
-    return () => {
-      dispatch(reset_add_test());
-    };
-  }, [dispatch]);
+	React.useEffect(() => {
+		return () => {
+			dispatch(reset_add_test());
+		};
+	}, [dispatch]);
 
-  function saveQuizName() {
-    const inputs = document.querySelectorAll("input, textarea");
-    const data = {
-      quiz_name: "",
-      questions: [],
-    };
-    Array.from(inputs).forEach((el) => {
-      let armel = el.name.split("-");
-      switch (armel[0]) {
-        case "quiz":
-          data[armel[1]] = el.value !== "on" ? el.value : el.checked;
-          break;
-        case "question":
-          if (data.questions[parseInt(armel[2])]) {
-            data.questions[parseInt(armel[2])][armel[1]] = el.checked
-              ? el.type == "checkbox"
-              : el.value;
-          } else {
-            data.questions[parseInt(armel[2])] = {
-              [armel[1]]: el.value !== "on" ? el.value : el.checked,
-              choices: [],
-            };
-          }
-          break;
-        case "choice":
-          if (
-            data.questions[parseInt(armel[2])]["choices"][parseInt(armel[3])]
-          ) {
-            data.questions[parseInt(armel[2])]["choices"][parseInt(armel[3])][
-              armel[1]
-            ] = el.value !== "on" ? el.value : el.checked;
-          } else {
-            data.questions[parseInt(armel[2])]["choices"][
-              parseInt(armel[3])
-            ] = { [armel[1]]: el.value !== "on" ? el.value : el.checked };
-          }
-          break;
-        default:
-          break;
-      }
-    });
+	function saveQuizName() {
+		const inputs = document.querySelectorAll('input, textarea');
+		const data = {
+			quiz_name: '',
+			questions: [],
+		};
+		Array.from(inputs).forEach((el) => {
+			let armel = el.name.split('-');
+			switch (armel[0]) {
+				case 'quiz':
+					data[armel[1]] = el.value !== 'on' ? el.value : el.checked;
+					break;
+				case 'question':
+					if (data.questions[parseInt(armel[2])]) {
+						data.questions[parseInt(armel[2])][armel[1]] = el.checked ? el.type == 'checkbox' : el.value;
+					} else {
+						data.questions[parseInt(armel[2])] = {
+							[armel[1]]: el.value !== 'on' ? el.value : el.checked,
+							choices: [],
+						};
+					}
+					break;
+				case 'choice':
+					if (data.questions[parseInt(armel[2])]['choices'][parseInt(armel[3])]) {
+						data.questions[parseInt(armel[2])]['choices'][parseInt(armel[3])][armel[1]] =
+							el.value !== 'on' ? el.value : el.checked;
+					} else {
+						data.questions[parseInt(armel[2])]['choices'][parseInt(armel[3])] = {
+							[armel[1]]: el.value !== 'on' ? el.value : el.checked,
+						};
+					}
+					break;
+				default:
+					break;
+			}
+		});
 
-    dispatch(add_test(data));
-  }
+		dispatch(add_test(data));
+	}
 
-  function addAnswers(el) {
-    setArray((prevArray) => [
-      ...prevArray.map((e) =>
-        e.question_id === el
-          ? {
-              ...e,
-              choises: [
-                ...e.choises,
-                {
-                  choise_id:
-                    prevArray[el].choises[prevArray[el].choises.length - 1]
-                      .choise_id + 1,
-                  isCorect: false,
-                },
-              ],
-            }
-          : e
-      ),
-    ]);
-  }
+	function addAnswers(el) {
+		setArray((prevArray) => [
+			...prevArray.map((e) =>
+				e.question_id === el
+					? {
+							...e,
+							choises: [
+								...e.choises,
+								{
+									choise_id: prevArray[el].choises[prevArray[el].choises.length - 1].choise_id + 1,
+									isCorect: false,
+								},
+							],
+					  }
+					: e
+			),
+		]);
+	}
 
-  function nextQuestion(n) {
-    return {
-      question_id: n,
-      choises: [
-        {
-          choise_id: 0,
-          isCorect: false,
-        },
-        {
-          choise_id: 1,
-          isCorect: false,
-        },
-      ],
-    };
-  }
+	function nextQuestion(n) {
+		return {
+			question_id: n,
+			choises: [
+				{
+					choise_id: 0,
+					isCorect: false,
+				},
+				{
+					choise_id: 1,
+					isCorect: false,
+				},
+			],
+		};
+	}
 
-  function addNewQuestion() {
-    setArray((prevArray) => [...prevArray, nextQuestion(prevArray.length)]);
-  }
-  if (test_status.status === "idle") {
-    return (
-      <div className="content_container">
-        <div className="content__">
-          <div className="title_container">
-            <p>Создать тест</p>
-          </div>
-          <div className="test-name_container">
-            <input
-              type="text"
-              name="quiz-quiz_name"
-              className="text-input"
-              placeholder="Название теста"
-            />
-          </div>
-          <div className="checkbox">
-            <input
-              id="is_public"
-              name="quiz-is_public"
-              type="checkbox"
-              className="inp"
-            />
-            <label className="labelforcheckbox" for="is_public">
-              Открьітьій тест?
-            </label>
-          </div>
-          <form name="quiz" className="questions_global_container">
-            {array.map((elQ) => {
-              return (
-                <QuestionComponent
-                  choices={elQ.choises}
-                  addAnswers={addAnswers}
-                  question_id={elQ.question_id}
-                />
-              );
-            })}
-          </form>
-          <div className="add-question-button_container">
-            <div
-              onClick={addNewQuestion}
-              className="add-question-button add-button button"
-            >
-              <div className="add-img">
-                <div></div>
-              </div>
-              <p>Добавить вопрос</p>
-            </div>
-          </div>
+	function addNewQuestion() {
+		setArray((prevArray) => [...prevArray, nextQuestion(prevArray.length)]);
+	}
 
-          <div className="create-button_container">
-            <div onClick={saveQuizName} className="create-button button">
-              <p>Создать</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-    ///Обработка ошибок 404
-  } else if (test_status.status === 404) {
-    return <p>ERRRROR</p>;
-    ///Екран Успешного добавления теста
-  } else if (test_status.status === 200) {
-    function copy() {
-      var msg = test_status.action.quiz_id;
-      var temp = document.createElement("textarea");
-      var tempMsg = document.createTextNode(msg);
-      temp.appendChild(tempMsg);
-      document.body.appendChild(temp);
-      temp.select();
-      document.execCommand("copy");
-      document.body.removeChild(temp);
-    }
-    return (
-      <div id="quiz_id" className="content__auth">
-        <div className="autorization_container">
-          <div className="title_container">
-            <p className="title">
-              Ід вашего теста: <br/>
-              <strong>{test_status.action.quiz_id}</strong>
-            </p>
-          </div>
-          <div className="button_container">
-            <input
-              onClick={copy}
-              type="button"
-              className="button_vote button"
-              value="Копировать"
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+	if (test_status.status === 'idle') {
+		return (
+			<div className="content_container">
+				<div className="content__">
+					<div className="title_container">
+						<p>Создать тест</p>
+					</div>
+					<div className="test-name_container">
+						<input type="text" name="quiz-quiz_name" className="text-input" placeholder="Название теста" />
+					</div>
+					<div className="checkbox">
+						<input id="is_public" name="quiz-is_public" type="checkbox" className="inp" />
+						<label className="labelforcheckbox" for="is_public">
+							Открытый тест?
+						</label>
+					</div>
+					<form name="quiz" className="questions_global_container">
+						{array.map((elQ) => {
+							return (
+								<QuestionComponent
+									setImage={setArray}
+									choices={elQ.choises}
+									addAnswers={addAnswers}
+									question_id={elQ.question_id}
+								/>
+							);
+						})}
+					</form>
+					<div className="add-question-button_container">
+						<div onClick={addNewQuestion} className="add-question-button add-button button">
+							<div className="add-img">
+								<div></div>
+							</div>
+							<p>Добавить вопрос</p>
+						</div>
+					</div>
+
+					<div className="create-button_container">
+						<div onClick={saveQuizName} className="create-button button">
+							<p>Создать</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+		///Обработка ошибок 404
+	} else if (test_status.status === 404) {
+		return <p>ERRRROR</p>;
+		///Екран Успешного добавления теста
+	} else if (test_status.status === 200) {
+		function copy() {
+			var msg = test_status.action.quiz_id;
+			var temp = document.createElement('textarea');
+			var tempMsg = document.createTextNode(msg);
+			temp.appendChild(tempMsg);
+			document.body.appendChild(temp);
+			temp.select();
+			document.execCommand('copy');
+			document.body.removeChild(temp);
+		}
+		return (
+			<div id="quiz_id" className="content__auth">
+				<div className="autorization_container">
+					<div className="title_container">
+						<p className="title">
+							Ід вашего теста: <br />
+							<strong>{test_status.action.quiz_id}</strong>
+						</p>
+					</div>
+					<div className="button_container">
+						<input onClick={copy} type="button" className="button_vote button" value="Копировать" />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
